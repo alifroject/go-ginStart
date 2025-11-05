695024faa9c21c33f0571d240d355d86c142589f
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-11-05 20:34:12 +0700

    change database and password

--- config/database.go
+++ config/database.go
@@ -3,15 +3,34 @@ package config
 import (
     "fmt"
     "log"
-    "gin-quickstart/models" 
+    "os"
+
+    "gin-quickstart/models"
     "gorm.io/driver/postgres"
     "gorm.io/gorm"
+
+    "github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-    dsn := "host=127.0.0.1 user=postgres password=123 dbname=gin_gorm port=5432 sslmode=disable"
+    // Load .env file
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    // Build DSN from environment variables
+    dsn := fmt.Sprintf(
+        "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+        os.Getenv("DB_HOST"),
+        os.Getenv("DB_USER"),
+        os.Getenv("DB_PASSWORD"),
+        os.Getenv("DB_NAME"),
+        os.Getenv("DB_PORT"),
+    )
+
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
         log.Fatal("Failed to connect to database:", err)
