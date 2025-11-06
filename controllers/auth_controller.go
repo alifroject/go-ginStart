3dad61ee68f945e5edf816f0a55bd838bab12eae
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-11-06 12:24:13 +0700

    Add auth controller

---
+++ controllers/auth_controller.go
@@ -0,0 +1,76 @@
+package controllers
+
+import (
+	"gin-quickstart/models"
+	"gin-quickstart/services"
+	"gin-quickstart/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SignupInput struct {
+	FirstName string `json:"firstName" binding:"required"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required"`
+}
+
+type LoginInput struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+func SignUp(c *gin.Context) {
+	var input SignupInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		utils.RespondError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	user := models.User{
+		FirstName: input.FirstName,
+		LastName:  input.LastName,
+		Email:     input.Email,
+		Password:  input.Password,
+	}
+
+	createdUser, err := services.SignUp(&user)
+	if err != nil {
+		utils.RespondError(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.RespondJSON(c, http.StatusCreated, createdUser)
+}
+
+func Login(c *gin.Context) {
+	var input LoginInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		utils.RespondError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, user, err := services.Login(input.Email, input.Password)
+	if err != nil {
+		utils.RespondError(c, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	utils.RespondJSON(c, http.StatusOK, gin.H{
+		"user":  user,
+		"token": token,
+	})
+
+}
+
+func GetMe(c *gin.Context) {
+    user, exists := c.Get("user")
+    if !exists {
+        c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found in context"})
+        return
+    }
+
+    c.JSON(http.StatusOK, user)
+}
+
