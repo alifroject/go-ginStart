beea44738f4e6d00f2c5e17bb61b4ea93a7b6c33
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-11-06 09:33:51 +0700

    fix

--- config/database.go
+++ config/database.go
@@ -1,45 +1,46 @@
 package config
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"os"
 
-    "gin-quickstart/models"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
+	//"gin-quickstart/models"  use in dev-site
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 
-    "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-    // Load .env file
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-    // Build DSN from environment variables
-    dsn := fmt.Sprintf(
-        "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-        os.Getenv("DB_HOST"),
-        os.Getenv("DB_USER"),
-        os.Getenv("DB_PASSWORD"),
-        os.Getenv("DB_NAME"),
-        os.Getenv("DB_PORT"),
-    )
+	// Build DSN from environment variables
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Failed to connect to database:", err)
-    }
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-    fmt.Println("Connected to PostgreSQL successfully!")
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
 
-    // Auto-migrate tables
-    db.AutoMigrate(&models.User{})
+	fmt.Println("Connected to PostgreSQL successfully!")
 
-    DB = db
+	// Auto-migrate tables
+	//  db.AutoMigrate(&models.User{}) uncomment this
+
+	DB = db
 }
