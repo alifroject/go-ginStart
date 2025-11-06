0a170071774cdfd1648a007f860152ebcc69f442
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-11-06 12:25:23 +0700

    add auth and admin

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -2,13 +2,17 @@ package routes
 
 import (
 	"gin-quickstart/controllers"
+	"gin-quickstart/middleware"
+
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterUserRoutes(router *gin.Engine) {
-	userGroup := router.Group("/users")
+	userGroup := router.Group("/user")
 	{
-		userGroup.GET("/", controllers.GetUsers)     
-		userGroup.POST("/", controllers.CreateUser)  
+		// GET /user - all users, admin only
+		userGroup.GET("/", middleware.AuthMiddleware(), middleware.AdminOnly(), controllers.GetUsers)
+		//no auth
+		userGroup.POST("/", controllers.CreateUser)
 	}
 }
