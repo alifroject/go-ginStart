d7ace90921b8ede6bcf423cc51fa13171960e05e
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-11-17 22:30:20 +0700

    Add basic product routes

---
+++ routes/product_routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"gin-quickstart/controllers"
+	"github.com/gin-gonic/gin"
+)
+func ProductRoutes(router *gin.Engine) { 
+	products := router.Group("/products")
+	{
+		products.POST("/", controllers.CreateProduct)
+		products.GET("/", controllers.GetProducts)
+		products.GET("/:id", controllers.GetProductByID)
+		products.PUT("/:id", controllers.UpdateProduct)
+		products.DELETE("/:id", controllers.DeleteProduct)
+	}
+}
