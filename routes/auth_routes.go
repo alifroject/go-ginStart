be328bdb028b1c0561ec6edda446e7eed923996c
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-11-18 19:16:45 +0700

    set admin middleware

--- routes/auth_routes.go
+++ routes/auth_routes.go
@@ -9,5 +9,14 @@ import (
 func RegisterAuthRoutes(router *gin.Engine) {
 	router.POST("/signup", controllers.SignUp)
 	router.POST("/login", controllers.Login)
+	router.POST("/admin/login", controllers.AdminLogin)
 	router.GET("/me", middleware.AuthMiddleware(), controllers.GetMe)
+
+	 // Protected admin-only routes
+    admin := router.Group("/admin")
+    admin.Use(middleware.AuthMiddleware(), middleware.AdminOnly())
+    {
+        admin.GET("/me", controllers.GetMe)
+        
+    }
 }
