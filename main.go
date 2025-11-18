9e65b129620bd7c583da0e8bdd49de8cca583595
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-11-18 21:50:10 +0700

    add cors for both sites(admin/user)

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"gin-quickstart/config"
 	"gin-quickstart/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func main() {
@@ -11,11 +13,22 @@ func main() {
 
 	router := gin.Default()
 
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:5173", // React admin panel
+			"http://localhost:8081", // Flutter web / testing origin
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
-    //routes
+	//routes
 	routes.RegisterRoutes(router)
-	routes.RegisterAuthRoutes(router) 
-
+	routes.RegisterAuthRoutes(router)
+	routes.ProductRoutes(router)
 
 	router.Run(":8080")
 }
